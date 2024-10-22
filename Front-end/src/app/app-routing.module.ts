e6a00a57f42c52a7058b247a3039b5ce4316e51f
Abdelrahman Tamer <168681286+AbdelrahmanTamer11@users.noreply.github.com>
2024-10-22 20:18:55 +0300

    Update_Frontend_without_Bootstrap

--- Front-end/src/app/app-routing.module.ts
+++ Front-end/src/app/app-routing.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SplashScreenComponent } from './splash-screen/splash-screen.component';
 import { LoginComponent } from './login/login.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
 
 const routes: Routes = [
+  { path: '', component: SplashScreenComponent },  // Default route for Splash Screen
   { path: 'login', component: LoginComponent },
   { path: 'sign-up', component: SignUpComponent },
-  { path: '', redirectTo: '/login', pathMatch: 'full' }, // Default route
 ];
 
 @NgModule({
---
+++ Front-end/src/app/splash-screen/splash-screen.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-splash-screen',
+  templateUrl: './splash-screen.component.html',
+  styleUrls: ['./splash-screen.component.css']
+})
+export class SplashScreenComponent implements OnInit {
+  displayedText = '';
+  fullText = 'Package Tracker';
+  charIndex = 0;
+
+  ngOnInit(): void {
+    this.animateText();
+  }
+
+  animateText() {
+    const interval = setInterval(() => {
+      if (this.charIndex < this.fullText.length) {
+        this.displayedText += this.fullText[this.charIndex];
+        this.charIndex++;
+      } else {
+        clearInterval(interval);
+      }
+    }, 100);  
+  }
+}
