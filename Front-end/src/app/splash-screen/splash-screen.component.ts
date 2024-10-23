cc6a16407e185c723ef3c30be8ba813e9418e346
Adham Aboelela <100962253+AdhamIsHere@users.noreply.github.com>
2024-10-23 20:21:32 +0300

    updated word animation

--- Front-end/src/app/splash-screen/splash-screen.component.ts
+++ Front-end/src/app/splash-screen/splash-screen.component.ts
@@ -20,8 +20,19 @@ export class SplashScreenComponent implements OnInit {
         this.displayedText += this.fullText[this.charIndex];
         this.charIndex++;
       } else {
+        // Clear interval when the animation finishes
         clearInterval(interval);
+
+        // Wait for 3 seconds before restarting the animation
+        setTimeout(() => {
+          // Reset values for the next loop
+          this.charIndex = 0;
+          this.displayedText = '';
+          // Start the animation again
+          this.animateText();
+        }, 3000)
       }
-    }, 100);  // Adjust speed of character appearance (100 ms between characters)
+    }, 150);
   }
+
 }
